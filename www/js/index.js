7367f02da7615862f3e921c3de31b5785d838c66
SergioEric <sericmorales@gmail.com>
2015-07-07 19:31:31 -0500

    all files

---
+++ www/js/index.js
@@ -0,0 +1,153 @@
+// function onBodyLoad(){
+	// document.addEventListener('deviceready"',isReady, false );
+
+  // function isReady(){
+    var App = angular.module('myApp', ['ionic']);
+// window.onload = function(){
+
+
+
+    App.run(function($ionicPlatform) {
+      $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+          // org.apache.cordova.statusbar required
+          StatusBar.styleLightContent();
+        }
+      });
+    });
+
+   App.config(function($stateProvider, $urlRouterProvider) {
+
+      // Ionic uses AngularUI Router which uses the concept of states
+      // Learn more here: https://github.com/angular-ui/ui-router
+      // Set up the various states which the app can be in.
+      // Each state's controller can be found in controllers.js
+      $stateProvider
+
+      // setup an abstract state for the tabs directive
+        .state('tab', {
+        url: "/tab",
+        abstract: true,
+        templateUrl: "templates/tabs.html"
+      })
+
+      // Each tab has its own nav history stack:
+
+      .state('tab.dash', {
+        url: '/dash',
+        views: {
+          'tab-dash': {
+            templateUrl: 'templates/tab-dash.html',
+            controller: 'DashCtrl'
+          }
+        }
+      })
+
+      .state('tab.chats', {
+          url: '/chats',
+          views: {
+            'tab-chats': {
+              templateUrl: 'templates/tab-chats.html',
+              controller: 'ChatsCtrl'
+            }
+          }
+        })
+        .state('tab.chat-detail', {
+          url: '/chats/:profileId',
+          views: {
+            'tab-chats': {
+              templateUrl: 'templates/chat-detail.html',
+              controller: 'ChatDetailCtrl'
+            }
+          }
+        })
+
+      .state('tab.account', {
+        url: '/account',
+        views: {
+          'tab-account': {
+            templateUrl: 'templates/tab-account.html',
+            controller: 'AccountCtrl'
+          }
+        }
+      })
+      .state('tab.vision',{
+        url: '/vision',
+        views:{
+          'tab-vision': {
+            templateUrl: 'templates/vision.html',
+            controller : 'VisionCtrl'
+          }
+        }
+      })
+
+      // if none of the above states are matched, use this as the fallback
+      $urlRouterProvider.otherwise('/tab/dash');
+
+    }); //end Config
+
+    /* Controllers */
+    App.controller('ChatsCtrl', function($scope, $http) {
+        // $scope.chats = Chats.all();
+      $http.get('js/profiles.json').success(function(data){
+        return $scope.profiles = data;
+      }).error(function(status){
+        alert(status)
+      });
+        $scope.remove = function(chat) {
+          $scope.profiles.remove(chat);
+        }
+    });
+
+    App.controller('ChatDetailCtrl', function($scope, $stateParams,$http) {
+    $http.get('js/profiles.json').success(function(data){
+        // $scope.chat = data
+        for(var i = 0; i < data.length; i++){
+          if (data[i].id === parseInt($stateParams.profileId)) {
+            return $scope.chat = data[i];
+          }
+        };
+        }).error(function(status){
+            alert(status)
+        });
+        // $scope.chat = Chats.get($stateParams.chatId);
+
+        $scope.doit = function(data){
+          alert('E-mail enviado');
+          console.log(data)
+        }
+    });
+
+    App.controller('AccountCtrl', function($scope) {
+      $scope.settings = {
+        enableFriends: true
+      };
+    });
+
+    App.controller('VisionCtrl',  function($scope) {
+        $scope.info = ['La vision es tal']
+    });
+
+      App.controller('DashCtrl',  function($scope, $http) {
+        $http.get('js/profiles.json').success(function(data){
+            $scope.profiles = data.length;
+        }).error(function(status){
+            alert(status)
+        });
+    });
+  // }
+
+    // End controllers
+    //
+    /* services*/
+
+// }
+
+// function isReady(){
+// 	alert('hi');
+// }
